906591374103f56c233c77a76e6ea2afd929e345
Albert Leitato <wizqydy@gmail.com>
2021-12-20 17:30:51 +0300

    Retur json response in example app and change image name

--- examples/hello-world/app/index.php
+++ examples/hello-world/app/index.php
@@ -1,3 +1,10 @@
 <?php
-phpinfo();
+    header('Content-Type: application/json');
+
+    echo json_encode([
+        "message" => "Hello, world",
+        "php" => phpversion(),
+        "query" => $_GET,
+        "headers" => getallheaders(),
+    ]);
 ?>
